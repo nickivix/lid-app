c548ef78a049026602291b68e78693e58be42e19
Nazar Shulika <shulika.02@gmail.com>
2023-03-28 17:15:17 +0200

    fix open

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import React, { useRef, useEffect, useState } from 'react'
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
 import { OrbitControls } from '@react-three/drei'
-import { Canvas, useFrame, useLoader } from '@react-three/fiber'
+import { Canvas, useLoader } from '@react-three/fiber'
 import './App.css'
+import { Euler, Vector3 } from 'three'
 
 function Model() {
   const gltf = useLoader(GLTFLoader, '/BOX.glb')
   const modelRef = useRef()
   const componentRefs = useRef([])
   const partRefs = useRef([])
-  const [materialState, setMaterialState] = useState({})
+  const [rotationState, setRotationState] = useState({})
+  const [positionState, setPositionState] = useState({})
 
   useEffect(() => {
     const components = gltf.scene.children
@@ -20,9 +22,13 @@ function Model() {
       for (let j = 0; j < component.children.length; j++) {
         const part = component.children[j]
         partRefs.current[i][j] = part
-        setMaterialState(prevState => ({
+        setRotationState(prevState => ({
           ...prevState,
-          [part.uuid]: part.material.clone(),
+          [part.uuid]: part.rotation.clone(),
+        }))
+        setPositionState(prevState => ({
+          ...prevState,
+          [part.uuid]: part.position.clone(),
         }))
       }
     }
@@ -30,14 +36,37 @@ function Model() {
 
   function handleClick(componentIndex, partIndex) {
     const part = partRefs.current[componentIndex][partIndex]
-    const material = materialState[part.uuid]
-    const newMaterial = material.clone()
-    newMaterial.transparent = true
-    newMaterial.opacity = material.opacity === 0 ? 1 : 0
-    setMaterialState(prevState => ({
+    const rotation = rotationState[part.uuid]
+    const position = positionState[part.uuid]
+    const newRotation = rotation.clone()
+    const newPosition = position.clone()
+
+    if (newRotation.equals(new Euler(-Math.PI / 3, 0, 0))) {
+      newRotation.set(0, 0, 0)
+    } else {
+      newRotation.set(-Math.PI / 3, 0, 0)
+    }
+
+    if (newPosition.equals(new Vector3(0, 80, -300))) {
+      newPosition.set(0, 0, 0)
+    } else {
+      newPosition.set(0, 80, -300)
+    }
+
+    setRotationState(prevState => ({
       ...prevState,
-      [part.uuid]: newMaterial,
+      [part.uuid]: newRotation,
     }))
+    setPositionState(prevState => ({
+      ...prevState,
+      [part.uuid]: newPosition,
+    }))
+
+    setRotationState(prevState => ({
+      ...prevState,
+      [part.uuid]: newRotation,
+    }))
+
   }
 
   return (
@@ -48,8 +77,15 @@ function Model() {
             <mesh
               key={partIndex}
               ref={partRefs.current[componentIndex]?.[partIndex]}
-              onClick={() => { if (partIndex === 0) handleClick(componentIndex, partIndex + 3) }}
-              material={materialState[part.uuid]}
+              onClick={() => {
+                if (partIndex === 0) {
+                  handleClick(componentIndex, partIndex + 3)
+                  handleClick(componentIndex, partIndex + 1)
+                }
+              }}
+              material={part.material}
+              position={positionState[part.uuid]}
+              rotation={rotationState[part.uuid]}
               geometry={part.geometry}
             />
           ))}
